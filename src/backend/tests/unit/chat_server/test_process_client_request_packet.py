0c0eb3ef6f663c673dbf589fb51f6015dbc806ac
Yurii <motovyuri@ya.ru>
2024-02-22 09:15:17 +0100

    chat_server send_message test

--- src/backend/tests/unit/chat_server/test_process_client_request_packet.py
+++ src/backend/tests/unit/chat_server/test_process_client_request_packet.py
@@ -9,7 +9,10 @@ from backend.models.chat_message import ChatUserMessage
 from backend.models.user_chat_link import UserChatLink
 from backend.schemas import client_packet as cli_p
 from backend.schemas import server_packet as srv_p
-from backend.schemas.chat_message import ChatUserMessageSchema
+from backend.schemas.chat_message import (
+    ChatUserMessageCreateSchema,
+    ChatUserMessageSchema,
+)
 from backend.services.chat_manager.chat_manager import ChatManager
 from backend.services.chat_server import process_client_request_packet
 
@@ -110,3 +113,35 @@ async def test_process_client_request_get_messages(
     for resp_msg in resp_messages:
         resp_msg_obj = ChatUserMessageSchema.model_validate_json(resp_msg)
         assert resp_msg_obj.text in expected_message_texts
+
+
+async def test_process_client_request_send_message(
+    chat_manager: ChatManager, async_session: AsyncSession
+):
+    user_id = uuid.uuid4()
+    chat_id = uuid.uuid4()
+    current_user_id = user_id
+
+    async_session.add(Chat(id=chat_id, title="my chat", owner_id=current_user_id))
+    async_session.add(UserChatLink(chat_id=chat_id, user_id=user_id))
+    await async_session.commit()
+
+    message = ChatUserMessageCreateSchema(
+        chat_id=chat_id, text="my message", sender_id=user_id
+    )
+    request = cli_p.ClientPacket(
+        id=random.randint(1, 10000), data=cli_p.CMDSendMessage(message=message)
+    )
+
+    response = await process_client_request_packet(
+        chat_manager=chat_manager, packet=request, current_user_id=current_user_id
+    )
+
+    assert isinstance(response.data, srv_p.ServerResponseSucessNoBody) is True
+    message_in_db = await async_session.scalar(
+        select(ChatUserMessage)
+        .where(ChatUserMessage.chat_id == chat_id)
+        .where(ChatUserMessage.sender_id == user_id)
+    )
+    assert message_in_db is not None
+    assert isinstance(message_in_db, ChatUserMessage)
