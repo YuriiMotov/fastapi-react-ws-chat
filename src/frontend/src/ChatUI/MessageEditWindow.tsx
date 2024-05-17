ab72926152f437de66ef9aee6ecafd279f780529
Yurii <motovyuri@ya.ru>
2024-05-17 16:08:07 +0200

    Split components from MessageList into separate modules

---
+++ src/frontend/src/ChatUI/MessageEditWindow.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import { ChatMessage } from "../ChatClient";
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Textarea,
+  ModalFooter,
+} from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
+
+
+interface MessageEditWindowComponentParams {
+  editedMessage: ChatMessage | null;
+  onMessageEditSave: (messageId: string, newText: string) => void;
+  resetEditedMessageHandler: () => void;
+}
+
+function MessageEditWindowComponent({
+  editedMessage,
+  onMessageEditSave,
+  resetEditedMessageHandler,
+}: MessageEditWindowComponentParams) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [editedMessageText, setEditedMessageText] = useState<string>("");
+
+  useEffect(() => {
+    if (editedMessage) {
+        setEditedMessageText(editedMessage.text);
+        onOpen();
+    }
+    return () => {
+        setEditedMessageText("");
+        onClose();
+    }
+  }, [editedMessage]);
+
+  function closeModal() {
+    resetEditedMessageHandler();
+    onClose();
+  }
+
+  function saveAndClose() {
+    onMessageEditSave(editedMessage!.id, editedMessageText);
+    closeModal();
+  }
+
+  return (
+    <Modal isOpen={isOpen} onClose={closeModal}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Message editing</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Textarea
+            id="message-edit-text"
+            value={editedMessageText}
+            onChange={(e)=>setEditedMessageText(e.target.value)}
+            isReadOnly={false}
+          />
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            colorScheme="telegram"
+            mr={3}
+            onClick={saveAndClose}
+          >
+            Save
+          </Button>
+          <Button variant="ghost" onClick={closeModal}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+
+export { MessageEditWindowComponent };
---
+++ src/frontend/src/ChatUI/MessageListItem.tsx
@@ -0,0 +1,65 @@
+import React, { memo } from "react";
+import { ChatMessage } from "../ChatClient";
+import {
+  Card,
+  CardHeader,
+  Flex,
+  Text,
+  CardBody,
+  Spacer,
+  Button,
+  Skeleton,
+} from "@chakra-ui/react";
+import { TiEdit } from "react-icons/ti";
+
+interface MessageListItemComponentParams {
+    message: ChatMessage;
+    currentUserID: string | null;
+    onShowEditMessageWindow: (message: ChatMessage) => void;
+}
+
+const MessageListItemComponent = memo(
+    function MessageListItemComponent({
+      message,
+      currentUserID,
+      onShowEditMessageWindow,
+    }: MessageListItemComponentParams) {
+      console.log(`repainting message ${message.id}`);
+      return (
+        <Card w="md" id={"chat-message-" + message.id}>
+          {!message.is_notification && (
+            <CardHeader
+              textColor="lightgray"
+              fontWeight="bold"
+              p="2"
+              paddingBottom="0"
+            >
+              <Flex direction="row">
+                <Skeleton height="20px" isLoaded={message.senderName != undefined}>
+                  <Text>{message.senderName || "Loading.."}</Text>
+                </Skeleton>
+                <Spacer />
+                {currentUserID === message.sender_id && (
+                  <Button
+                    rightIcon={<TiEdit />}
+                    variant="link"
+                    onClick={() =>
+                      onShowEditMessageWindow(message)
+                    }
+                  />
+                )}
+              </Flex>
+            </CardHeader>
+          )}
+          <CardBody>
+            <Text whiteSpace='pre-line'>
+              {message.text}
+            </Text>
+          </CardBody>
+        </Card>
+      );
+    }
+);
+
+
+export { MessageListItemComponent };
